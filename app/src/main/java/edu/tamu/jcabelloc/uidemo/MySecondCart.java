2cc38b2da2ad056602af6859bb5e079e8b5a9282
Juan Cabello <jcabelloc@gmail.com>
2018-06-06 20:26:19 -0500

    ### Make an activity a searchable activity without creating new activities

--- app/src/main/java/edu/tamu/jcabelloc/uidemo/MySecondCart.java
+++ app/src/main/java/edu/tamu/jcabelloc/uidemo/MySecondCart.java
@@ -2,14 +2,17 @@ package edu.tamu.jcabelloc.uidemo;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.SearchView;
 
 public class MySecondCart extends AppCompatActivity {
 
+    SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +28,37 @@ public class MySecondCart extends AppCompatActivity {
 
 
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) findViewById(R.id.prodSearchView); //menu.findItem(R.id.search).getActionView();
+        searchView = (SearchView) findViewById(R.id.prodSearchView); //menu.findItem(R.id.search).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setQueryHint("Search Product");
+        //searchView.setQueryHint("Search Product");
         searchView.setIconifiedByDefault(false);
         searchView.setSubmitButtonEnabled(true);
 
+
+        // Receiving the query
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.d("Search inside OnCreate:",query );
+        }
+
+        handleIntent(getIntent());
     }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        searchView.setQuery("", false);
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.d("Search inside newIntent" ,query );;
+        }
+    }
+
 }
